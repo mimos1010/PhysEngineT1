54df88c0e3821ba391de843b299355fceeb57aef
mimos1010 <zwitztheone@gmail.com>
2016-05-11 18:55:35 -0400

    Create InputCheck.h

---
+++ InputCheck.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "mainprojinc.h"
+#include "PShape.h"
+
+class InputCheck
+{
+public:
+	InputCheck();
+	~InputCheck();
+
+	//main input checking function
+
+	void CheckInputs(RenderWindow &windowx);
+
+	//public variables
+
+
+};
+
