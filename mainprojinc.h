8a9ea619a12bd2ea8cd4b69f2bfcff9d63da2cd8
mimos1010 <zwitztheone@gmail.com>
2016-05-11 18:54:33 -0400

    Create mainprojinc.h

---
+++ mainprojinc.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <stdlib.h>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <SFML\Graphics.hpp>
+#include <SFML\Window.hpp>
+#include <SFML\Main.hpp>
+
+using namespace std;
+using namespace sf;
+
+//public global constants
+
+const int DEFAULT_WIN_WIDTH = 800;
+const int DEFAULT_WIN_HEIGHT = 640;
+const string WIN_NAME = "Physics Engine 0.1";
+
