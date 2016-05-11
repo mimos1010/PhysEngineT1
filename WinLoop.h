b306214166ab46d14ab7464555511d4f83d64f62
mimos1010 <zwitztheone@gmail.com>
2016-05-11 18:55:04 -0400

    Create WinLoop.h

---
+++ WinLoop.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "mainprojinc.h"
+#include "InputCheck.h"
+
+class WinLoop
+{
+public:
+	WinLoop();
+	~WinLoop();
+	
+	//Main Window Loop function
+
+	void MainWinLoop(RenderWindow &window);
+
+	//public variables
+
+};
+
